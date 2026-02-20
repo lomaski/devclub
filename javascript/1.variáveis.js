21055074b4faa31961d55e01ef2a11be6440a345
lomaski <lomaski1986@gmail.com>
2026-02-20 14:14:34 -0300

    new file:   "javascript/1.vari\303\241veis.js" 	new file:   javascript/2.tipoDaDados.js 	new file:   "javascript/M\303\263dulo 1" 	new file:   javascript/ex00.js

---
+++ javascript/1.variáveis.js
@@ -0,0 +1,12 @@
+/*
+variáveis
+- let
+- const
+- var -> desconcernoado
+*/
+
+let abacaxi = 40
+
+const x = 80
+
+console.log(abacaxi)
---
+++ javascript/2.tipoDaDados.js
@@ -0,0 +1,10 @@
+/*
+1 - string -> texto
+*/
+const texto1 = "aspas duplas"
+const texto2 = 'aspas simples'
+const texto3 = ` crase `
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
