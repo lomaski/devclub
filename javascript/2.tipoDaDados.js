2fdc26dee28a1526f4991a8fbcedcc5053403b1b
lomaski <lomaski1986@gmail.com>
2026-02-23 07:54:40 -0300

    modified:   javascript/2.tipoDaDados.js 	new file:   javascript/3.ifElse.js 	new file:   javascript/4.function.js

--- javascript/2.tipoDaDados.js
+++ javascript/2.tipoDaDados.js
@@ -1,5 +1,12 @@
 /*
 1 - string -> texto
+2 - numbers -> números
+3 - Boolean -> boleando
+4 - Object -> objetos
+5 - Null & Undefined 
+-----
+
+1 - Array 
 */
 const texto1 = "aspas duplas"
 const texto2 = 'aspas simples'
@@ -8,3 +15,23 @@ const texto3 = ` crase `
 console.log(texto1)
 console.log(texto2)
 console.log(texto3)
+
+const myAge = 30
+const myString = `Minha idade é ${myAge}` // Template Lite
+console.log (myString)
+
+const pessoa = {
+    name : 'Ayesha',
+    nasc : '02/10/1986',
+    address: {
+        street: "Rua Condessa de São Joaquim",
+        number: 254,
+        neighborhood: 'Bela Vista',
+        city: 'São Paulo',
+        state: "SP",
+        country: 'Brasil'
+    }
+}
+
+console.log(pessoa.address.number)
+
---
+++ javascript/3.ifElse.js
@@ -0,0 +1,20 @@
+/*
+Controlador de Fluxo
+IF(Se) -> Faça ISSO
+Else (Se não)
+
+Operadores de comparação
+> maior que
+< menor que
+== igual que
+
+*/
+
+const notaDoAluno = 10
+const notaDeCorte = 5
+
+if (notaDoAluno > notaDeCorte) {
+    console.log("Parabéns, você passou de ano")
+}else{
+    console.log("Você foi reprovado")
+}
