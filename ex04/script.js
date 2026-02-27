fb3299f645d90a8999555f8f6d2daf3005b6f20c
lomaski <lomaski1986@gmail.com>
2026-02-27 10:48:14 -0300

    modified:   ex04/index.html 	modified:   ex04/script.js 	modified:   ex04/style.css

---
+++ ex04/script.js
@@ -0,0 +1,12 @@
+let coin = document.querySelectorAll('.coin')
+
+
+function clique(){
+    console.log(coin[0].value)
+    console.log(coin[1].value)
+    console.log('cliquei!')
+}
+
+function digitei(){
+    console.log('digitei...')
+}
