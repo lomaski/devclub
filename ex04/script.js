302bd2b12c3cc16ab0a62df8db1bd1117a1134c0
lomaski <lomaski1986@gmail.com>
2026-03-01 14:39:13 -0300

    modified:   ex04/script.js 	modified:   ex04/style.css

--- ex04/script.js
+++ ex04/script.js
@@ -1,12 +1,19 @@
 let coin = document.querySelectorAll('.coin')
+let valor = document.querySelector('.section-input')
 
 
 function clique(){
     console.log(coin[0].value)
     console.log(coin[1].value)
+    console.log(valor.value)
     console.log('cliquei!')
 }
 
 function digitei(){
-    console.log('digitei...')
+    console.log(coin[0].value)
+    console.log(coin[1].value)
 }
+
+
+coin[0].addEventListener("click", digitei)
+coin[1].addEventListener("click", digitei)
