6341e005b5b08e810b3df28c81da2778e806c0a5
lomaski <lomaski1986@gmail.com>
2026-03-02 14:32:20 -0300

    modified:   ex04/index.html 	modified:   ex04/script.js 	modified:   ex04/style.css

--- ex04/script.js
+++ ex04/script.js
@@ -1,17 +1,53 @@
 let coin = document.querySelectorAll('.coin')
 let valor = document.querySelector('.section-input')
+let div = document.querySelectorAll('.moeda-div')
+let coin1 = coin[0].className
+let coin2 = coin[1].className
+
+
+const moeda = [
+    br = [
+        'R$',
+        'Real',
+        'br'
+    ],
+    usa = [
+        'US$',
+        'Dolar',
+        'eua'
+    ],
+    euro = [
+        '€',
+        'Euro',
+        'euro'
+    ],
+    uk = [
+        '£',
+        'Libra',
+        'uk'
+    ],
+    bi = [
+        '₿',
+        'Bitcoin',
+        'Bitcoin'
+    ]
+]
+
 
 
 function clique(){
-    console.log(coin[0].value)
-    console.log(coin[1].value)
-    console.log(valor.value)
-    console.log('cliquei!')
+    coin1 = coin[0].className
+    coin2 = coin[1].className
+
+    num = valor.value
+
+    console.log(coin1+' + '+coin2+' + '+num)
 }
 
 function digitei(){
     console.log(coin[0].value)
     console.log(coin[1].value)
+    console.log(valor.value)
 }
 
 
